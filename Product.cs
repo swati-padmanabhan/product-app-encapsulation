e51c142caafba13bead6744564bd67c745391a4a
Swati P <67189695+swati-padmanabhan@users.noreply.github.com>
2024-07-11 23:11:28 +0530

    initial commit of both class file & test class
    Both Class file that is "Product.cs" as well as "Program.cs" has been uploaded

---
+++ Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductAppDemo.models
+{
+    internal class Product
+    {
+        private int _id;
+        private string _name;
+        private double _price;
+        private double _discountPercentage;
+
+        public int GetId() { return _id; }
+        public string GetName() { return _name; }
+        public double GetPrice() { return _price; }
+        public double GetDiscountPercentage() { return _discountPercentage; }
+
+        public void SetId(int id) { _id = id; }
+        public void SetName(string name) { _name = name; }
+        public void SetPrice(double price) { _price = price; }
+        public void SetDiscountPercentage(double discountPercentage) { _discountPercentage = discountPercentage; }
+
+
+        public double PriceAfterDiscount()
+        {
+            double discountedAmount = (_discountPercentage / 100) * _price;
+            return _price-discountedAmount;
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductAppDemo.models;
+
+namespace ProductAppDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product productNo1 = new Product();
+            productNo1.SetId(1);
+            productNo1.SetName("Sephora");
+            productNo1.SetPrice(20000);
+            productNo1.SetDiscountPercentage(20);
+
+            Product productNo2 = new Product();
+            productNo2.SetId(2);
+            productNo2.SetName("Huda Beauty");
+            productNo2.SetPrice(10000);
+            productNo2.SetDiscountPercentage(10);
+
+            Console.WriteLine("Details of first product is: ");
+            Console.WriteLine("Product ID is: " + productNo1.GetId());
+            Console.WriteLine("Product NAME is: " + productNo1.GetName());
+            Console.WriteLine("Product's ACTUAL PRICE is: " + productNo1.GetPrice());
+            Console.WriteLine("Product's DISCOUNTED PRICE is: " + productNo1.PriceAfterDiscount());
+            Console.WriteLine("=================================================================");
+
+            Console.WriteLine("Details of second product is: ");
+            Console.WriteLine("Product ID is: " + productNo2.GetId());
+            Console.WriteLine("Product NAME is: " + productNo2.GetName());
+            Console.WriteLine("Product's ACTUAL PRICE is: " + productNo2.GetPrice());
+            Console.WriteLine("Product's DISCOUNTED PRICE is: " + productNo2.PriceAfterDiscount());
+
+
+        }
+    }
+}
